0d988841d3b25666dfa461323031fbc855b454fb
Ahmed Asaad <ahmedasaadarn@gmail.com>
2026-08-15 17:10:39 +0400

    I Fixed a a bug in the login page

--- middlewares/errorHandler.middleware.js
+++ middlewares/errorHandler.middleware.js
@@ -10,6 +10,13 @@ export const errorHandler = (err, req, res, next) => {
       error: err.message,
     });
   }
+   if (err.name === 'CastError') {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid ID format',
+      error: err.message,
+    });
+  }
 
   if (err.code === 11000) {
     return res.status(409).json({
--- routes/index.route.js
+++ routes/index.route.js
@@ -13,14 +13,8 @@ router.use('/cases',  caseRoute);
 export default router;
 
 
-/*eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjZhNzE4NTE3YzFhNjYwYmVmNDc2M2JmZiIsImZ1bGxOYW1lIjoiQWhtZWQgU2FsaW0iLCJlbXBsb3llZUlkIjoiMjAwMjEiLCJyb2xlIjoiQ2xlcmsiLCJpYXQiOjE3ODU4MjQ1MzYsImV4cCI6MTc4NTkxMDkzNn0.L4NZ4h6VCJVqYX4yhgybzCxBMNXmGrwJOBCy5lTIH98*/
 
 
 
 
-/*
-Change the the mongDB assigned to the user  from just id to mongoId becasue the mixup with the employeeid 
-check the hearing date and In hearing realtionship and model it.
 
-
-*/
